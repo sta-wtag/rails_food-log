4d256abbba91bbe0608153333fca0b22dafdc149
“sta-wtag” <sadia.tabassum@welldev.io>
2023-07-04 13:57:22 +0600

    implement api to create,update,delete items

--- app/api/items.rb
+++ app/api/items.rb
@@ -6,10 +6,17 @@ class Items < Grape::API
     format :json
     desc 'End-points for shop products'
     namespace :items do
+        desc 'Get all items'
         get do
             items = Item.all
             present items, with: Entities::ItemEntity
         end
+        desc 'Get specific item'
+        get ':id' do
+            item = Item.find(params[:id])
+            present item, with: Entities::ItemEntity 
+        end  
+        desc 'Create new item'   
         params do
             requires :name, type: String
             requires :description, type: String
@@ -17,9 +24,35 @@ class Items < Grape::API
             requires :available_quantity, type: Integer
         end
         post do
-            item = Item.create(params)
-
+            item = Item.create!(params)
             present item, with: Entities::ItemEntity
-        end    
+        end 
+        desc 'Update specific item'
+        params do
+            requires :id, type: Integer 
+            optional :name, type: String
+            optional :description, type: String
+            optional :price, type: Integer
+            optional :available_quantity, type: Integer 
+        end
+        patch ':id' do 
+            item = Item.find(params[:id])
+            item.update!({
+                name: params[:name] || item.name,
+                description: params[:description] || item.description,
+                price: params[:price] || item.price,
+                available_quantity: params[:available_quantity] || item.available_quantity
+            })
+            present item,  with: Entities::ItemEntity
+        end
+        desc 'Delete specific item'
+        params do
+            requires :id, type: Integer 
+        end
+        delete ':id' do
+            item = Item.find(params[:id])
+            item.destroy
+            present item,  with: Entities::ItemEntity
+        end         
     end    
 end    
