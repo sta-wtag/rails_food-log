7fb966517bbd90ff340ab395f992bfe24106ead1
“sta-wtag” <sadia.tabassum@welldev.io>
2023-07-05 17:25:07 +0600

    add sing out button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
+
 class ApplicationController < ActionController::Base
-  
+  def current_user
+    if doorkeeper_token.present?
+        @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+    else
+        @current_user = User.find_by(id:1)
+    end
+  end 
 end
