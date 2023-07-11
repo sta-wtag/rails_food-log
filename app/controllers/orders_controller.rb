80af6fd6fd57f0c2da496dd99a90ccb84b0f4527
“sta-wtag” <sadia.tabassum@welldev.io>
2023-07-11 17:56:08 +0600

    create order view, show order view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,8 +15,27 @@ class OrdersController < ApplicationController
     end
 
     def create 
+
         @order = Order.new(order_params)
+        total_price = 0
+        
+        params[:ordered_items].each do |order_item|
+          if order_item[:id]
+            item = Item.find(order_item[:id])
+            total_price += item.price * order_item[:quantity].to_i
+          end
+        end
+        
+        @order.total_price = total_price
+
         if @order.save
+          params[:ordered_items].each do |order_item|
+            if order_item[:id]
+              item = Item.find(order_item[:id])
+              item.update(available_quantity:item.available_quantity-order_item[:quantity].to_i)
+              createOrderedItem(@order.id, item, order_item[:quantity].to_i)
+            end
+          end
           flash[:success] = "Order successfully created"
           redirect_to @order
         else
@@ -40,7 +59,11 @@ class OrdersController < ApplicationController
         end
     end 
     def order_params
-        params.require(:order).permit(order_items_attributes: [:item_id, :quantity])
+        params.require(:order).permit( :user_id,:total_price, order_items_attributes: [:item_id, :quantity])
     end
+    def createOrderedItem(orderID, item, quantity)
+      binding.pry
+      OrderedItem.create(order_id: orderID, item_id: item.id, quantity: quantity, price: item.price)
+    end  
   
 end    
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
   has_many :authentication_tokens
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attr_accessor :name
+  
 
   after_initialize :set_default_role, if: :new_record?
   def self.authenticate(email,password)
     user = User.find_for_authentication(email: email)
-    binding.pry
     user&.valid_password?(password) ? user : nil
   end 
   private
